879a61c2912586f333d0b2c94a7559c1c5bd8967
tkniki <trankimniki@gmail.com>
2022-11-06 11:10:16 +0100

    Models for database created: IndoorData, OutdoorData, complex type Temperature

---
+++ Weather_Monitor/Models/IndoorData.cs
@@ -0,0 +1,10 @@
+﻿namespace Weather_Monitor.Models
+{
+    public class IndoorData
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public Temperature Temperature { get; set; }
+        public int Humidity { get; set; }
+    }
+}
---
+++ Weather_Monitor/Models/OutdoorData.cs
@@ -0,0 +1,8 @@
+﻿namespace Weather_Monitor.Models
+{
+    public class OutdoorData
+    {
+        public int Id { get; set; }
+        public Temperature Temperature { get; set; }
+    }
+}
