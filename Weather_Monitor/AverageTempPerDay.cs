3854377037fb3885a0ce62fd53f95fffe2338782
tkniki <trankimniki@gmail.com>
2022-11-26 13:30:06 +0100

    display average temperature per day

---
+++ Weather_Monitor/AverageTempPerDay.cs
@@ -0,0 +1,16 @@
+﻿namespace Weather_Monitor
+{
+    public class AverageTempPerDay
+    {
+        public DateTime Date { get; set; }
+        public double AverageTempInside { get; set; }
+        public double AverageTempOutside { get; set; }
+
+        public AverageTempPerDay(DateTime date, double averageTempInside, double averageTempOutside)
+        {
+            Date = date;
+            AverageTempInside = averageTempInside;
+            AverageTempOutside = averageTempOutside;
+        }
+    }
+}
